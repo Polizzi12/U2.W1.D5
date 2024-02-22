26540d5ebcd615a7622f2db4ae17e4660d93b920
Polizzi12 <peppepolizzi73@gmail.com>
2024-02-22 22:56:57 +0100

    tutto fixato

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const nav = document.querySelector("container1");
+const nav = document.getElementById("container1");
+const button = document.getElementById("boxblack");
+
 
 function scrollfum(){
     if (window.scrollY > 350 ){
-        nav.Style.backgroundColor = "white";
+        nav.style= "background-color: white; transition: all 0.5s;"
+        button.style= "background-color: green; transition: all 0.5s;"
     }else{
-        nav.Style.backgroundColor= "#ffc017";
+        nav.style="background-color: #ffc017; transition: all 0.5s;";
+        button.style="background-color: black; trnasition: all 0.5s;";
     }
 }
 window.addEventListener("scroll", scrollfum);
